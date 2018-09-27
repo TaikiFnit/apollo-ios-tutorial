7de7f12175bc4d2cd9e7b8a3f0824ab8b701d18d
TaikiFnit <taikifnitspg@gmail.com>
2018-09-27 18:59:02 +0900

    apolloでLocal Serverからデータを取ってみた

--- apollo-test/ViewController.swift
+++ apollo-test/ViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import Apollo
+
+struct Book: Codable {
+    let title: String?
+    let author: String?
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let apollo = ApolloClient(url: URL(string: "http://localhost:4000")!)
+        apollo.fetch(query: BooksQuery()) { (result, error) in
+            guard let books = result?.data?.books else {
+                return
+            }
+            
+            self.label.text = books.map { books in "\(books?.title ?? "") by \(books?.author ?? "")"}.joined(separator: "\n")
+        }
     }
-
-
 }
 

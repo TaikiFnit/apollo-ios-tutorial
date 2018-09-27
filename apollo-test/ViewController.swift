f82385b6a60d83c5018da43f1f8c5781a42fced1
TaikiFnit <taikifnitspg@gmail.com>
2018-09-27 16:19:04 +0900

    Initial Commit

---
+++ apollo-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  apollo-test
+//
+//  Created by TaikiFnit on 2018/09/27.
+//  Copyright © 2018 TaikiFnit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ apollo-testTests/apollo_testTests.swift
@@ -0,0 +1,34 @@
+//
+//  apollo_testTests.swift
+//  apollo-testTests
+//
+//  Created by TaikiFnit on 2018/09/27.
+//  Copyright © 2018 TaikiFnit. All rights reserved.
+//
+
+import XCTest
+@testable import apollo_test
+
+class apollo_testTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
